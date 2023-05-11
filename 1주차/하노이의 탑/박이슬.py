08c2c89de34b8077f81937b5ab68d5822a356eb7
Suling <omjl5123@catholic.ac.kr>
2023-05-11 14:15:48 +0900

    1주차 하노이의 탑 박이슬

---
+++ 1주차/하노이의 탑/박이슬.py
@@ -0,0 +1,14 @@
+def solution(n):
+    answer = []
+    
+    def hanoi(n, start, via, end):
+        if n == 1:
+            answer.append([start, end])
+        else:
+            hanoi(n - 1, start, end, via)
+            answer.append([start, end])
+            hanoi(n - 1, via, start, end)
+        
+    hanoi(n, 1, 2, 3)
+    
+    return answer
