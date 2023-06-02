94f7fdb4318ad52538632b148c42a61130c7acd4
Yiseul Park <omjl5123@catholic.ac.kr>
2023-06-02 11:21:10 +0900

    add: 5주차_정수 삼각형_박이슬

---
+++ 5주차/정수 삼각형/박이슬.py
@@ -0,0 +1,10 @@
+def solution(triangle):
+    n = len(triangle)
+    dp = [[0] * n for _ in range(n)]
+    dp[n - 1] = triangle[-1]
+    
+    for i in range(n - 2, -1, -1):
+        for j in range(i + 1):
+            dp[i][j] = max(dp[i + 1][j], dp[i + 1][j + 1]) + triangle[i][j]
+    
+    return dp[0][0]
